1369816a42ec1f0102fb890d956891abf71f409c
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-17 16:51:02 -0700

    adds Activity and ActivityContext

---
+++ src/Components/Activity.jsx
@@ -0,0 +1,14 @@
+import { ActivityContextProvider } from "../Context/ActivityContext";
+import Question from "./Question";
+import QuestionFooter from "./QuestionFooter";
+import QuestionHeader from "./QuestionHeader";
+
+export default function Activity() {
+  return (
+    <ActivityContextProvider>
+      <QuestionHeader />
+      <Question />
+      <QuestionFooter />
+    </ActivityContextProvider>
+  );
+}
---
+++ src/Context/ActivityContext.jsx
@@ -0,0 +1,44 @@
+import { useContext, createContext, useState } from "react";
+
+// step 1: create default state
+export const defaultState = {
+  answer: 0,
+  currentQuestionID: 0,
+  totalScore: 0,
+  showLearnMoreModal: false,
+  setAnswer: () => {},
+  setCurrentQuestion: () => {},
+  setTotalScore: () => {},
+  setShowLearnMoreModal: () => {},
+};
+
+// step 2: create context
+export const ActivityContext = createContext(defaultState);
+
+// step 3: create use context
+export const useActivityContext = () => useContext(ActivityContext);
+
+// step 4: create the provider
+export const ActivityContextProvider = ({ children }) => {
+  const [answer, setAnswer] = useState(0);
+  const [currentQuestionID, setCurrentQuestionID] = useState(0);
+  const [totalScore, setTotalScore] = useState(0);
+  const [showLearnMoreModal, setShowLearnMoreModal] = useState(false);
+  const value = {
+    answer,
+    setAnswer,
+    currentQuestionID,
+    setCurrentQuestionID,
+    totalScore,
+    setTotalScore,
+    showLearnMoreModal,
+    setShowLearnMoreModal,
+  };
+  return (
+    <ActivityContext.Provider value={value}>
+      {children}
+    </ActivityContext.Provider>
+  );
+};
+
+// step 5: use provider in component (see Activity.jsx)
