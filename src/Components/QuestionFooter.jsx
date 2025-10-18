cdcd16d73977c5082320a204f0d04ac8920b8f8c
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-17 17:13:43 -0700

    Adds question Header and Footer

---
+++ src/Components/QuestionFooter.jsx
@@ -0,0 +1,8 @@
+export default function QuestionFooter() {
+  return (
+    <p>
+      Source/Credits: All image attributions and topic resources will be
+      provided at the end of the mission.
+    </p>
+  );
+}
---
+++ src/Components/QuestionHeader.jsx
@@ -0,0 +1,19 @@
+import { useActivityContext } from "../Context/ActivityContext";
+
+export default function QuestionHeader() {
+  const { currentQuestionID } = useActivityContext();
+  function handleOnClick() {
+    // TODO
+  }
+
+  return (
+    <div className="QuestionHeader">
+      <div className="CurrentQuestionNumber">
+        <img src="/" alt="globe icon"></img>
+        <h2>Question {currentQuestionID} of 10</h2>
+        <p>Different Cultures Trivia</p>
+      </div>
+      <button onClick={handleOnClick}>Exit Mission</button>
+    </div>
+  );
+}
