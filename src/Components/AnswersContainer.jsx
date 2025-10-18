10a44ff8db9dfb34d01cbed609179590078f74b1
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-17 23:15:20 -0700

    Removes unnecessary Answer component

--- src/Components/AnswersContainer.jsx
+++ src/Components/AnswersContainer.jsx
@@ -1,15 +1,21 @@
 import { DIFFERENT_CULTURES_TRIVIA } from "../constants";
 import { useActivityContext } from "../Context/ActivityContext";
-import Answer from "./Answer";
 
 export default function AnswersContainer() {
   const { currentQuestionID } = useActivityContext();
   const currentQuestion = DIFFERENT_CULTURES_TRIVIA[currentQuestionID];
+  function handleOnClick() {
+    //TODO
+  }
 
   return (
     <div className="AnswersContainer">
       {currentQuestion.answers.map(({ id, text, isCorrect }) => (
-        <Answer text={text} key={id} isCorrect={isCorrect} />
+        <button handleOnClick>
+          {/* TODO: Talk with design team to verify that these are icons */}
+          <img src="/" alt="Icon" />
+          {text}
+        </button>
       ))}
     </div>
   );
