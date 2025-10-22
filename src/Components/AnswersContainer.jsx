4122e1ebeda6aab751db8b4b058ed9f8e9b24d58
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-21 20:46:44 -0700

    Adds conditional formatting for selected answer button

--- src/Components/AnswersContainer.jsx
+++ src/Components/AnswersContainer.jsx
@@ -2,19 +2,20 @@ import { DIFFERENT_CULTURES_TRIVIA } from "../constants";
 import { useActivityContext } from "../Context/ActivityContext";
 
 export default function AnswersContainer() {
-  const { currentQuestionID } = useActivityContext();
+  const { currentQuestionID, answer, setAnswer } = useActivityContext();
   const currentQuestion = DIFFERENT_CULTURES_TRIVIA[currentQuestionID];
-  function handleOnClick() {
-    //TODO
-  }
 
   return (
     <div className="AnswersContainer">
       {currentQuestion.answers.map(({ id, text, isCorrect }) => (
         <button
           key={id}
-          className="unselectedAnswerButton answerButton"
-          onClick={handleOnClick}
+          className={
+            id == answer
+              ? "selectedAnswerButton"
+              : "unselectedAnswerButton answerButton"
+          }
+          onClick={() => setAnswer(id)}
         >
           <img
             src={"src\\assets\\images\\icons\\option" + id + ".svg"}
--- src/Context/ActivityContext.jsx
+++ src/Context/ActivityContext.jsx
@@ -2,7 +2,7 @@ import { useContext, createContext, useState } from "react";
 
 // step 1: create default state
 export const defaultState = {
-  answer: 0,
+  answer: null,
   currentQuestionID: 0,
   totalScore: 0,
   showLearnMoreModal: false,
@@ -20,7 +20,7 @@ export const useActivityContext = () => useContext(ActivityContext);
 
 // step 4: create the provider
 export const ActivityContextProvider = ({ children }) => {
-  const [answer, setAnswer] = useState(0);
+  const [answer, setAnswer] = useState(null);
   const [currentQuestionID, setCurrentQuestionID] = useState(0);
   const [totalScore, setTotalScore] = useState(0);
   const [showLearnMoreModal, setShowLearnMoreModal] = useState(false);
