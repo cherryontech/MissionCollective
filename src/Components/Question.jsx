ea19f91bca8768a6c031ebef7c743e44d1eb1007
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-24 16:43:14 -0700

    Imports images so they display properly

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -2,6 +2,45 @@ import { CULTURE_QUESTIONS } from "../data/cultureQuestions";
 import { useActivityContext } from "../Context/ActivityContext";
 import AnswersContainer from "./AnswersContainer";
 import LearnMoreModal from "./LearnMoreModal";
+//TODO: Figure out how to use relative paths instead of importing images
+import image1 from "../assets/images/quiz1/1.jpg";
+import image2 from "../assets/images/quiz1/2.jpg";
+import image3 from "../assets/images/quiz1/3.jpg";
+import image4 from "../assets/images/quiz1/4.jpg";
+import image5 from "../assets/images/quiz1/5.jpg";
+import image6 from "../assets/images/quiz1/6.png";
+import image7 from "../assets/images/quiz1/7.jpg";
+import image8 from "../assets/images/quiz1/8.jpg";
+import image9 from "../assets/images/quiz1/9.jpg";
+import image10 from "../assets/images/quiz1/10.jpg";
+
+export function getImageSrc(id) {
+  const imageID = id + 1;
+  switch (imageID) {
+    case 1:
+      return image1;
+    case 2:
+      return image2;
+    case 3:
+      return image3;
+    case 4:
+      return image4;
+    case 5:
+      return image5;
+    case 6:
+      return image6;
+    case 7:
+      return image7;
+    case 8:
+      return image8;
+    case 9:
+      return image9;
+    case 10:
+      return image10;
+    default:
+      return;
+  }
+}
 
 export default function Question() {
   const { currentQuestionID, setCurrentQuestionID, answer, updateAnswerMap } =
@@ -24,7 +63,10 @@ export default function Question() {
     <div className="Question">
       <div className="QuestionContainer">
         <p>{currentQuestion.questionText}</p>
-        <img src={currentQuestion.imageURL} alt={currentQuestion.altText}></img>
+        <img
+          src={getImageSrc(currentQuestionID)}
+          alt={currentQuestion.altText}
+        ></img>
         <AnswersContainer />
       </div>
       <div className="QuestionNavigation">
