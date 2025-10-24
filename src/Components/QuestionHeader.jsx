ff756692e4fc512571947e652b8b1d791522f27e
anabel <anabelcurry@gmail.com>
2025-10-23 21:33:29 -0700

    adding additional styling

--- src/Components/QuestionHeader.jsx
+++ src/Components/QuestionHeader.jsx
@@ -1,5 +1,6 @@
 import { useActivityContext } from "../Context/ActivityContext";
 import GlobeIcon from "../assets/globe-icon.svg";
+import ExitArrowIcon from "../assets/exit-arrow.svg";
 import "../styles/QuestionHeader.css";
 
 export default function QuestionHeader() {
@@ -17,10 +18,20 @@ export default function QuestionHeader() {
           height="40"
           width="40"
         ></img>
-        <h2>Question {currentQuestionID + 1} of 10</h2>
-        <p>Different Cultures Trivia</p>
+        <div className="CurrentQuestionNumberText">
+          <h2>Question {currentQuestionID + 1} of 10</h2>
+          <p>Different Cultures Trivia</p>
+        </div>
       </div>
-      <button onClick={handleOnClick}>Exit Mission</button>
+      <div className="ExitMissionButton">
+        <button onClick={handleOnClick}>Exit Mission</button>
+        <img
+          src={ExitArrowIcon}
+          alt="globe icon"
+          height="40"
+          width="40"
+        ></img>
+      </div>
     </div>
   );
 }
