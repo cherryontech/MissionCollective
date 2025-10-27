5bfdaf8436633dd4c7047d4844359e6fd7e7e7ca
jennycodingnow <101082675+jennycodingnow@users.noreply.github.com>
2025-10-27 10:49:05 -0700

    Implement learnmoremodal component + Styling (#12)
    * Implement LearnMoreModal and add styling
    
    * Update display message according to wireframe
    
    * Update to reflect PR comments
    
    * Update styling reflect PR comment
    
    * Copy icons from main for feature work
    
    * Update to add the icons to display messages
    
    * Updated displayText with icons
    
    * Updated questions file per PR comment
    
    * Update styling, messages, and add reset answer per PR comments
    
    * Update styling add background overlay

--- src/Components/LearnMoreModal.jsx
+++ src/Components/LearnMoreModal.jsx
@@ -1,32 +1,70 @@
 import { useActivityContext } from "../Context/ActivityContext";
-
-import { DIFFERENT_CULTURES_TRIVIA } from "../constants";
+import { CULTURE_QUESTIONS } from "../data/cultureQuestions";
+import correctLight from "../assets/icons/correctLight.svg";
+import incorrectLight from "../assets/icons/incorrectLight.svg";
+import "./LearnMoreModal.css";
 
 export default function LearnMoreModal() {
   const {
     currentQuestionID,
     setCurrentQuestionID,
     showLearnMoreModal,
+    answerMap,
+    setAnswer,
     setShowLearnMoreModal,
   } = useActivityContext();
 
-  const displayText = {
-    correct: "Correct! Well done!",
-    incorrect: "Incorrect, but nice try though!",
-  };
-
-  function handleOnClick() {
-    // this should update the currentQuestionID and set showLearnMoreModal to false
+  if (!showLearnMoreModal) {
+    return null;
   }
 
-  // this should only display if showLearnMoreModal is true
+  const currentQuestion = CULTURE_QUESTIONS[currentQuestionID];
+  const selectedAnswerId = answerMap[currentQuestionID];
+
+  const selectedAnswer = currentQuestion.answerOptions.find(
+    (answer) => answer.id === selectedAnswerId
+  );
+
+  let isCorrect;
+  if (selectedAnswer && selectedAnswer.isCorrect !== null) {
+    isCorrect = selectedAnswer.isCorrect;
+  } else {
+    isCorrect = false;
+  }
+
+  const displayText = {
+    correct: "Correct! You've earned full points for your team.",
+    incorrect: "Incorrect, but you still earned partial points for the team!",
+  };
+
+  // TODO: Need logic to handle the case when the user is on the last question in later sprint.
+  const handleContinue = () => {
+    setAnswer(null);
+    setCurrentQuestionID((id) => id + 1);
+    setShowLearnMoreModal(false);
+  };
+
   return (
-    <div className="LearnMoreModal">
-      {/* the text in h3 should change conditionally depending on if the answer was right or not */}
-      <h3>{displayText.correct}</h3>
-      <h4>Learn More:</h4>
-      <p>{DIFFERENT_CULTURES_TRIVIA[currentQuestionID].learnMoreText}</p>
-      <button onClick={handleOnClick}>Continue</button>
+    <div className="LearnMoreModalOverlayer">
+      <div className="LearnMoreModalContainer">
+        <div className="IsCorrectBanner">
+          <h3 className={isCorrect ? "CorrectText" : "IncorrectText"}>
+            <img
+              src={isCorrect ? correctLight : incorrectLight}
+              alt={isCorrect ? "Correct" : "Incorrect"}
+              className="IsCorrectIcon"
+            />
+            {isCorrect ? displayText.correct : displayText.incorrect}
+          </h3>
+        </div>
+        <div className="LearnMoreText">
+          <h4>Learn More:</h4>
+          <p>{currentQuestion.learnMoreText}</p>
+        </div>
+        <button className="ContinueButton" onClick={handleContinue}>
+          Continue
+        </button>
+      </div>
     </div>
   );
 }
