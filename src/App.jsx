ca1de6bcb5ce06fd6256bce395a0c3f7a44cf414
Jenny C <jennyhc888@gmail.com>
2025-10-31 14:21:21 -0700

    Add NavBar and HomePage to setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Activity from "./Components/Activity";
+import HomePage from "./Components/HomePage";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/activity" element={<Activity />} />
