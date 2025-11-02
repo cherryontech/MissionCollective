2149429f6cb21e9facb192eb31f22361c97ed10c
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-01 18:04:19 -0700

    Adds header styling

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/icons/logo.svg";
+import "../styles/NavBar.css";
 
 export default function NavBar() {
   return (
     <nav>
-      <img src={logo} alt="logo" />
-      <Link to="/">Home</Link>
-      <a href="">How It Works</a>
-      <Link to="/activity">Explore Missions</Link>
-      <a href="">Pricing</a>
-      <a href="">About Us</a>
-      <button>Log In</button>
+      <div className="menu">
+        <img src={logo} alt="logo" />
+        <Link to="/">Home</Link>
+        <a href="">How It Works</a>
+        <Link to="/activity">Explore Missions</Link>
+        <a href="">Pricing</a>
+        <a href="">About Us</a>
+      </div>
+      <div className="userInfo">
+        <button className="loginButton">Log In</button>
+      </div>
     </nav>
   );
 }
