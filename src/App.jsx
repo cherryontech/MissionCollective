4526fc873e58569acf7785d180f916511c92278d
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-01 21:56:27 -0700

    Clean up CSS (#15)
    * Moves reused button styling to App
    
    * Adds styling
    
    * Changes for consistency
    
    * Changes for consistency
    
    * Adds offset for button focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./styles/App.css";
 import Activity from "./Components/Activity";
 import HomePage from "./Components/HomePage";
 import NavBar from "./Components/NavBar";
