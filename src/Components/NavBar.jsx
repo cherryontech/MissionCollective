39d2bc57625e7425d9e219dc22b7e32700375a25
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-03 15:49:54 -0800

    Adds active link styling to nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/icons/logo.svg";
 import "../styles/NavBar.css";
 
@@ -8,11 +8,36 @@ export default function NavBar() {
       <div className="navElements">
         <div className="menu">
           <img src={logo} alt="logo" />
-          <Link to="/">Home</Link>
-          <a href="">How It Works</a>
-          <Link to="/activity">Explore Missions</Link>
-          <a href="">Pricing</a>
-          <a href="">About Us</a>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+          >
+            Home
+          </NavLink>
+          <a
+            href=""
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+          >
+            How It Works
+          </a>
+          <NavLink
+            to="/activity"
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+          >
+            Explore Missions
+          </NavLink>
+          <a
+            href=""
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+          >
+            Pricing
+          </a>
+          <a
+            href=""
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+          >
+            About Us
+          </a>
         </div>
         <div className="userInfo">
           <button className="loginButton">Log In</button>
