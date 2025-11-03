94b7153010c30c2aea890fa672c77fb094deb775
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-03 14:59:15 -0800

    Fixes footer styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,16 +2,18 @@ import "../styles/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <a href="" title="Coming Soon">
-        Help
-      </a>
-      <a href="" title="Coming Soon">
-        Privacy
-      </a>
-      <a href="" title="Coming Soon">
-        Terms
-      </a>
+    <div className="footerContainer">
+      <div className="footerElements">
+        <a href="" title="Coming Soon">
+          Help
+        </a>
+        <a href="" title="Coming Soon">
+          Privacy
+        </a>
+        <a href="" title="Coming Soon">
+          Terms
+        </a>
+      </div>
     </div>
   );
 }
