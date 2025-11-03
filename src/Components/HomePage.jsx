ea80bf2847cd5bd4d3bac3626187c0b29055d0aa
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-02 22:09:18 -0800

    Tries to fix width

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Footer from "./Footer";
 
 const content = {
@@ -28,13 +27,6 @@ export default function HomePage() {
         <h2>{content.heading5}</h2>
       </div>
       <Footer />
-=======
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is just a placeholder page to test out routes setup.</p>
->>>>>>> 4526fc873e58569acf7785d180f916511c92278d
     </div>
   );
 }
