e2c556582eafdb469d9554fc3b6094bf5757159a
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-06 18:26:23 -0800

    Updates section numbering

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,19 +2,22 @@ import Footer from "./Footer";
 import "../styles/HomePage.css";
 
 const content = {
-  heading1: "Building a Better Team, Together",
-  heading2: "Why Team-Building Fails (And We Don't)",
-  heading3: "Points with a Purpose",
-  heading4: "What Teams Are Saying",
-  heading5: "Ready to Build a Thriving Community?",
-  button1: "Start a Team Mission",
-  button2: "Start a Free Trial",
-  button3: "Explore Missions",
+  heading0: "Building a Better Team, Together",
+  heading1: "Why Team-Building Fails (And We Don't)",
+  heading2: "Points with a Purpose",
+  heading3: "What Teams Are Saying",
+  heading4: "Ready to Build a Thriving Community?",
+  button0: "Start a Team Mission",
+  button1: "Start a Free Trial",
+  button2: "Explore Missions",
 };
 
 export default function HomePage() {
   return (
     <div>
+      <div className="section0">
+        <h2>{content.heading0}</h2>
+      </div>
       <div className="section1">
         <h2>{content.heading1}</h2>
       </div>
@@ -26,12 +29,9 @@ export default function HomePage() {
       </div>
       <div className="section4">
         <h2>{content.heading4}</h2>
-      </div>
-      <div className="section5">
-        <h2>{content.heading5}</h2>
         <div className="buttonContainer">
-          <button className="primaryButton">{content.button2}</button>
-          <button className="ghostButton">{content.button3}</button>
+          <button className="primaryButton">{content.button1}</button>
+          <button className="ghostButton">{content.button2}</button>
         </div>
       </div>
       <Footer />
