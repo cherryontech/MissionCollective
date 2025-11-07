04b61f314acb8756eca55b26e72e0b5c32c7ba9d
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-06 18:15:27 -0800

    Add homepage navigation and footer (#18)
    * Moves reused button styling to App
    
    * Adds styling
    
    * Changes for consistency
    
    * Changes for consistency
    
    * Adds offset for button focus
    
    * Adds changes from Add routes PR
    
    * Adds elements to header and footer
    
    * Adds styling for footer
    
    * Adds header styling
    
    * Adjusts widths
    
    * Adds icons
    
    * Tries to fix width
    
    * Fixes overflow
    
    * Fixes footer styling
    
    * Fixes NavBar positioning
    
    * Adds active link styling to nav bar
    
    * Fixes missing import
    
    * Updates links and fixes styling
    
    * Adds favicon and website title

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footerContainer">
+      <div className="footerElements">
+        <a title="Coming Soon">Help</a>
+        <a title="Coming Soon">Privacy</a>
+        <a title="Coming Soon">Terms</a>
+      </div>
+    </div>
+  );
+}
