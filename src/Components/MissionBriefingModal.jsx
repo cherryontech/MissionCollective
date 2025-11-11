9ed2a5c972510e17262ff81f4c138407711d9597
Jenny C <jennyhc888@gmail.com>
2025-11-10 19:45:22 -0800

    Scaffolded Mission Briefing Modal

---
+++ src/Components/MissionBriefingModal.jsx
@@ -0,0 +1,27 @@
+import ".../Styles/MissionBriefingModal.css";
+
+export default function MissionBriefingModal() {
+  return (
+    <div className="MissionBriefingModalOverlayer">
+      <div className="MissionBriefingModalContainer">
+        <div>
+          <img></img>
+          <h3>Different Cultures Trivia</h3>
+          <p>Ready to explore and earn points?</p>
+        </div>
+        <div>
+          <h4></h4>
+          <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ol>
+        </div>
+        <div>
+          <button>Go Back</button>
+          <button>Let's Start</button>
+        </div>
+      </div>
+    </div>
+  );
+}
