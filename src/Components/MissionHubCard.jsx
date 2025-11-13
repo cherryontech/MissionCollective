c5040b00c95e2e95cf2f7a8afc38cebe5873c259
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-12 16:11:01 -0800

    Removes hardcoded text

--- src/Components/MissionHubCard.jsx
+++ src/Components/MissionHubCard.jsx
@@ -1,19 +1,16 @@
-import calendar from "../assets/icons/missionHub/calendar.svg";
-import completed from "../assets/icons/missionHub/completed.svg";
-import idea from "../assets/icons/missionHub/idea.svg";
-import masks from "../assets/icons/missionHub/masks.svg";
-import olympics from "../assets/icons/missionHub/olympics.svg";
-import scale from "../assets/icons/missionHub/scale.svg";
-import team from "../assets/icons/missionHub/team.svg";
-import world from "../assets/icons/missionHub/world.svg";
 import clock from "../assets/icons/missionHub/clock.svg";
 import starCircle from "../assets/icons/starCircle.svg";
 import { cardContent } from "../data/missionHubCards";
 
+const buttonText = {
+  beforeActivity: "Mission Briefing",
+  afterActivity: "View Team Contributions",
+};
+
 export default function MissionHubCard() {
   return (
     <div>
-      <img src={calendar} alt="calendar icon" />
+      <img src={cardContent[0].icon} alt={cardContent[0].alt} />
       <h2>{cardContent[0].headline}</h2>
       <p>{cardContent[0].body}</p>
       <div>
@@ -24,7 +21,7 @@ export default function MissionHubCard() {
         <img src={starCircle} alt="star icon" />
         <p>{cardContent[0].numCompleted}/5 teammates completed</p>
       </div>
-      <button>Button Text</button>
+      <button>{buttonText.beforeActivity}</button>
     </div>
   );
 }
