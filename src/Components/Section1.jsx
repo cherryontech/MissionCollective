e6e841a7e7013f6f9918e5f93171b5620baef1dc
jennycodingnow <101082675+jennycodingnow@users.noreply.github.com>
2025-11-14 13:08:06 -0800

    Homepage-Rsst-76 (#19)
    * Add homepage part 3 and part 5
    
    * Add homepage draft
    
    * Add icons for homepage
    
    * Add profile icon for homepage
    
    * Add more profile pics for homepage
    
    * Fix styling
    
    * Added styling
    
    * Add styling to homepage
    
    * Add styling to homepage
    
    * Added styling to homepage
    
    * Fix css styling
    
    * Add footer back
    
    * Fixing margin between nav and main
    
    * Refactor codes
    
    * Fixed width of cards
    
    * Fixed width of cards in section 3 for better alignment.
    
    * Added section4 back
    
    * Fix avatars spacing

---
+++ src/Components/Section1.jsx
@@ -0,0 +1,31 @@
+import CardsSteps from "./CardsSteps";
+import { MISSIONSTEPS } from "../data/homepageData.js";
+
+export default function Section1() {
+  return (
+    <section className="section1">
+      <div className="homepage-container">
+        <h2>
+          Why Team-Building Fails <span>(And We Don't)</span>
+        </h2>
+        <p className="connection-text">
+          <span>Real Connection in 3 Simple Steps: </span>
+          Mission Collective is built on the belief that genuine connection
+          requires learning and respect. We provide the structure, your team
+          builds the trust.
+        </p>
+        <div className="card-container">
+          {MISSIONSTEPS.map((card) => (
+            <CardsSteps
+              key={card.id}
+              id={card.id}
+              icon={card.icon}
+              title={card.title}
+              text={card.text}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
