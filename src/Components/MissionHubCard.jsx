7f41e47da89066d9432a436ed85af7e5803146dd
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-14 21:29:54 -0800

    Adds changes from other branches

--- src/Components/MissionHubCard.jsx
+++ src/Components/MissionHubCard.jsx
@@ -3,6 +3,11 @@ import clock from "../assets/icons/missionHub/clock.svg";
 import starCircle from "../assets/icons/starCircle.svg";
 import { cardContent } from "../data/missionHubCards";
 
+const buttonText = {
+  beforeActivity: "View Mission Briefing",
+  afterActivity: "View Team Contributions",
+};
+
 export default function MissionHubCard() {
   return (
     <div className="cardContainer">
@@ -25,7 +30,7 @@ export default function MissionHubCard() {
         <img src={starCircle} alt="star icon" className="inlineIcon" />
         <p>{cardContent[0].numCompleted}/5 teammates completed</p>
       </div>
-      <button>Button Text</button>
+      <button>{buttonText.beforeActivity}</button>
     </div>
   );
 }
