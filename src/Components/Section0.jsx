5605c97d980f72ade5609cbca762b4407c2ad592
anabel <anabelcurry@gmail.com>
2025-11-16 15:56:19 -0800

    more updates to section 0

--- src/Components/Section0.jsx
+++ src/Components/Section0.jsx
@@ -4,19 +4,26 @@ export default function Section0() {
   return (
     <section className="section0">
       <div className="homepage-container">
-        <h2>
-          Building A Better Team, <span>Together</span>
-        </h2>
+        <div className="section0-heading">
+          <h1>Building A Better Team, Together</h1>
+          <p>
+            True collaboration starts with understanding, and we’re here to 
+            help you build that deeper connection.
+          </p>
+        </div>
         <div className="img-section-content">
           <img src={highFive} alt="coworkers-high-fiving"></img>
-          <p className="connection-text">
-            <span>Mission Collective </span>
-            replaces dry team-building with purpose-driven fun. Our dedicated 
-            virtual space helps modern teams genuinely connect through positive, 
-            gamified trivia and collaborative challenges. Discover diverse 
-            personal backgrounds in a low-pressure environment, transforming your 
-            group into a thriving community. Start a Team Mission
-          </p>
+          <div className="section0-text">
+            <p>
+              <span>Mission Collective </span>
+              replaces dry team-building with purpose-driven fun. Our dedicated 
+              virtual space helps modern teams genuinely connect through positive, 
+              gamified trivia and collaborative challenges. Discover diverse 
+              personal backgrounds in a low-pressure environment, transforming your 
+              group into a thriving community. Start a Team Mission
+            </p>
+            <button>Start Next Mission</button>
+          </div>
         </div>
       </div>
     </section>
