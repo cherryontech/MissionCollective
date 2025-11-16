43115fe0f68a866d3db3d20c82436cf1ec4d507c
anabel <anabelcurry@gmail.com>
2025-11-16 13:00:43 -0800

    add new images and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
-import Activity from "./Components/Activity";
-import HomePage from "./Components/HomePage";
-import NavBar from "./Components/NavBar";
-import MissionHub from "./Components/MissionHub";
+import Activity from "./components/activity/Activity";
+import HomePage from "./components/homepage/HomePage";
+import NavBar from "./components/NavBar";
+import MissionHub from "./components/MissionHub";
 
 function App() {
   return (
