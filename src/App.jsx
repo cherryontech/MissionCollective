83e3552e13ccbea86db8cf287df272eff2c32372
anabel <anabelcurry@gmail.com>
2025-11-16 11:17:07 -0800

    fix app.jsx file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
-import Activity from "./components/Activity/Activity";
-import HomePage from "./Components/Homepage/HomePage";
+import Activity from "./components/activity/Activity";
+import HomePage from "./Components/homepage/HomePage";
 import NavBar from "./Components/NavBar";
 import MissionHub from "./Components/MissionHub";
 
