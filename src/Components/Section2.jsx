f2ef5f233e0aebc96d67fda487d0988862ddbc36
anabel <anabelcurry@gmail.com>
2025-11-17 11:11:45 -0800

    finish section 2

--- src/Components/Section2.jsx
+++ src/Components/Section2.jsx
@@ -9,11 +9,10 @@ export default function Section2() {
           <div className="section2-text">
             <h2> Points with a Purpose </h2>
             <p>
-              <span>Mission Collective </span>
-              points are always earned for the whole team. Use them to fund a 
-              collective reward that matters, like a shared training course, a 
-              team lunch, or a donation to a group-chosen charity. The 
-              possibilities are endless! 
+              <span>Mission Collective </span> points are always earned for the 
+              whole team. Use them to fund a collective reward that matters, 
+              like a shared training course, a team lunch, or a donation to a 
+              group-chosen charity. The possibilities are endless! 
             </p>            
           </div>
         </div>
