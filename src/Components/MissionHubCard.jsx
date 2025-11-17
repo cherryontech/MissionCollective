5210d16e045d8c7815fddc6524808411a0020a40
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-17 13:00:41 -0800

    Create Mission Hub Card component scaffolding (#27)
    * Creates mission hub card
    
    * Adds images and card data
    
    * Removes hardcoded text
    
    * Updates button text

--- src/Components/MissionHubCard.jsx
+++ src/Components/MissionHubCard.jsx
@@ -1,11 +1,27 @@
+import clock from "../assets/icons/missionHub/clock.svg";
+import starCircle from "../assets/icons/starCircle.svg";
+import { cardContent } from "../data/missionHubCards";
+
+const buttonText = {
+  beforeActivity: "View Mission Briefing",
+  afterActivity: "View Team Contributions",
+};
+
 export default function MissionHubCard() {
   return (
     <div>
-      <h2>Card Headline</h2>
-      <p>Card Body</p>
-      <p>Mission Info</p>
-      <p>#/# teammates completed</p>
-      <button>Button Text</button>
+      <img src={cardContent[0].icon} alt={cardContent[0].alt} />
+      <h2>{cardContent[0].headline}</h2>
+      <p>{cardContent[0].body}</p>
+      <div>
+        <img src={clock} alt="clock icon" />
+        <p>{cardContent[0].info}</p>
+      </div>
+      <div>
+        <img src={starCircle} alt="star icon" />
+        <p>{cardContent[0].numCompleted}/5 teammates completed</p>
+      </div>
+      <button>{buttonText.beforeActivity}</button>
     </div>
   );
 }
