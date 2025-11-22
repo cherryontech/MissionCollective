8abc9d8469cbfdd639f4f50e25273538f88895ac
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-21 16:49:18 -0800

    Adds styling to Mission Hub page

--- src/Components/MissionHub.jsx
+++ src/Components/MissionHub.jsx
@@ -1,45 +1,48 @@
 import Footer from "./Footer";
 import MissionHubCard from "./MissionHubCard";
 import { cardContent } from "../data/missionHubCards";
+import "../styles/MissionHub.css";
 
 const content = {
   heading: "Launch Your Next Mission",
   subheading:
-    "Choose an activity to collaborate with your team and unlock shared rewards",
+    "Choose an activity to collaborate with your team and unlock shared rewards.",
 };
 
 export default function MissionHub() {
   return (
-    <div>
-      <h1>{content.heading}</h1>
-      <p className="subtitle1">{content.subheading}</p>
-      <div className="missionCards">
-        {cardContent.map(
-          ({
-            id,
-            icon,
-            alt,
-            headline,
-            body,
-            minutes,
-            points,
-            numCompleted,
-          }) => (
-            <MissionHubCard
-              key={id}
-              id={id}
-              icon={icon}
-              alt={alt}
-              headline={headline}
-              body={body}
-              minutes={minutes}
-              points={points}
-              numCompleted={numCompleted}
-            />
-          )
-        )}
+    <>
+      <div className="missionHub">
+        <h1 className="heading">{content.heading}</h1>
+        <p className="subheading">{content.subheading}</p>
+        <div className="missionCards">
+          {cardContent.map(
+            ({
+              id,
+              icon,
+              alt,
+              headline,
+              body,
+              minutes,
+              points,
+              numCompleted,
+            }) => (
+              <MissionHubCard
+                key={id}
+                id={id}
+                icon={icon}
+                alt={alt}
+                headline={headline}
+                body={body}
+                minutes={minutes}
+                points={points}
+                numCompleted={numCompleted}
+              />
+            )
+          )}
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
