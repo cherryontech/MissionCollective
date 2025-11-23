819880b42c3591176b22d5f192ead43cf44b0cc2
Jenny C <jennyhc888@gmail.com>
2025-11-22 19:20:30 -0800

    Add login nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,20 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import logo from "../assets/icons/logo.svg";
 import "../styles/NavBar.css";
+import NavUserSection from "./NavUserSection";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // TODO: Update to link to the dashboard after activity has been completed
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+    navigate("/mission-hub");
+  };
+
   return (
     <nav>
       <div className="navElements">
@@ -39,10 +51,14 @@ export default function NavBar() {
             About Us
           </a>
         </div>
-        <div className="userInfo">
-          <Link to="/mission-hub">
-            <button className="loginButton">Log In</button>
-          </Link>
+        <div>
+          {isLoggedIn ? (
+            <NavUserSection />
+          ) : (
+            <button onClick={handleLoginClick} className="loginButton">
+              Log In
+            </button>
+          )}
         </div>
       </div>
     </nav>
