0dd2850a778349a72c55f5a1b41b24ec16251385
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-24 00:50:16 -0800

    Implements tracking quiz score

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -3,6 +3,8 @@ import { useContext, createContext, useState } from "react";
 export const defaultState = {
   activityCompleted: false,
   setActivityCompleted: () => {},
+  quizScore: 0,
+  setQuizScore: () => {},
 };
 
 export const UserContext = createContext(defaultState);
@@ -11,9 +13,12 @@ export const useUserContext = () => useContext(UserContext);
 
 export const UserContextProvider = ({ children }) => {
   const [activityCompleted, setActivityCompleted] = useState(false);
+  const [quizScore, setQuizScore] = useState(0);
   const value = {
     activityCompleted,
     setActivityCompleted,
+    quizScore,
+    setQuizScore,
   };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
