b3a3cef6a1367adb651323b564dd179e28cfc61b
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-25 23:02:14 -0800

    Add stat card scaffolding (#54)
    * Adds Dashboard
    
    * Adds stat card

---
+++ src/data/statCards.js
@@ -0,0 +1,35 @@
+import book from "../assets/icons/dashboard/book.svg";
+import calendar from "../assets/icons/dashboard/calendar.svg";
+import people from "../assets/icons/dashboard/people.svg";
+import star from "../assets/icons/dashboard/star.svg";
+
+export const statCardContent = [
+  {
+    id: 0,
+    icon: star,
+    alt: "star icon",
+    number: 1772,
+    stat: "Points Earned",
+  },
+  {
+    id: 1,
+    icon: people,
+    alt: "people icon",
+    number: 246,
+    stat: "Total Missions",
+  },
+  {
+    id: 2,
+    icon: book,
+    alt: "book icon",
+    number: 326,
+    stat: "Questions Answered",
+  },
+  {
+    id: 3,
+    icon: calendar,
+    alt: "calendar icon",
+    number: 85,
+    stat: "Rewards Progress",
+  },
+];
