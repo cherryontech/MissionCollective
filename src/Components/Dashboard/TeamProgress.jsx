a69d0c706557ef7b3ebe96cc1c5dad4cd0285c24
anabel <anabelcurry@gmail.com>
2025-11-30 14:19:42 -0800

    add initial scaffolding

--- src/Components/Dashboard/TeamProgress.jsx
+++ src/Components/Dashboard/TeamProgress.jsx
@@ -1,3 +1,19 @@
+import { teamProgressContent } from "../../data/teamProgress";
+
 export default function TeamProgress() {
-  return <h2>Your Team Progress</h2>;
+  return (
+    <div className="team-progress-container">
+      <h2>Your Team Progress</h2>
+      <div className="team-members">
+        {teamProgressContent.map(({ name, role, points, img }, idx) => (
+          <div className="team-member" key={`${name}-${idx}`}>
+            <img src={img} alt={name} />
+            <p className="member-name">{name}</p>
+            <p className="member-role">{role}</p>
+            <p className="member-points">{points}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
