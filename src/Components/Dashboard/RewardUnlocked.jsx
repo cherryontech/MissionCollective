3ae95c2ddf32c578a0ac0f588bdc032afef0a886
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-01 00:33:28 -0800

    fixes positioning

--- src/Components/Dashboard/RewardUnlocked.jsx
+++ src/Components/Dashboard/RewardUnlocked.jsx
@@ -1,21 +1,30 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import "../../styles/RewardUnlocked.css";
 
+const layout = {
+  fit: "fit-height",
+  align: [0.5, 0.5],
+};
+
 export default function RewardUnlocked() {
   return (
     <div className="rewardUnlockedContainer">
       <h1>Reward Unlocked!</h1>
-      <DotLottieReact
-        src="https://lottie.host/2b6403b7-3511-447a-8f22-cfdfdabfc500/RUbDVg5LNJ.lottie"
-        loop
-        autoplay
-      />
-      <DotLottieReact
-        src="https://lottie.host/95bab056-ba9d-44e2-ba99-b59140a3d6d2/eTRFWLrkDy.lottie"
-        loop
-        autoplay
-      />
-      <div>
+      <div className="animations">
+        <DotLottieReact
+          src="https://lottie.host/2b6403b7-3511-447a-8f22-cfdfdabfc500/RUbDVg5LNJ.lottie"
+          loop
+          autoplay
+          layout={layout}
+        />
+        <DotLottieReact
+          src="https://lottie.host/95bab056-ba9d-44e2-ba99-b59140a3d6d2/eTRFWLrkDy.lottie"
+          loop
+          autoplay
+          layout={layout}
+        />
+      </div>
+      <div className="rewardUnlockedButtons">
         <button className="secondaryButton">Set New Goal</button>
         <button>Claim Reward</button>
       </div>
