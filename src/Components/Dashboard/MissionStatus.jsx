f51faa35b52f38b8c6902670816ae089f91fa895
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-01 14:31:56 -0800

    Adds conditional rendering of mission status

--- src/Components/Dashboard/MissionStatus.jsx
+++ src/Components/Dashboard/MissionStatus.jsx
@@ -1,9 +1,18 @@
 import RewardUnlocked from "./RewardUnlocked";
+import { useUserContext } from "../../Context/UserContext";
 
 export default function MissionStatus() {
-  return (
-    <div className="missionStatusContainer">
-      <RewardUnlocked />
-    </div>
-  );
+  const { activityCompleted, missionCompleted } = useUserContext();
+
+  if (missionCompleted) {
+    return (
+      <div className="missionStatusContainer">
+        <RewardUnlocked />
+      </div>
+    );
+  } else if (activityCompleted) {
+    return <h1>Mission Accomplished!</h1>;
+  } else {
+    return <h1>Start a New Mission</h1>;
+  }
 }
