2351f85ccc713c98811015121d4074f6aa8eef13
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-01 01:33:14 -0800

    Adds functionality

--- src/Components/Dashboard/StatCard.jsx
+++ src/Components/Dashboard/StatCard.jsx
@@ -1,21 +1,40 @@
 import "../../styles/StatCard.css";
+import { useUserContext } from "../../Context/UserContext";
 
-function numberToString(id, number) {
+// TODO: Update this function when we add condition for unlocking reward
+function numberToString(id, number, activityCompleted, quizScore) {
   let stringNum = "";
-  if (id == 3) {
-    stringNum = number.toString() + "%";
-  } else {
-    stringNum = number.toLocaleString("en-US");
+
+  switch (id) {
+    case 0:
+      stringNum = (number + quizScore).toLocaleString("en-US");
+      break;
+    case 1:
+      stringNum = (number + 1).toLocaleString("en-US");
+      break;
+    case 2:
+      stringNum = (number + 10).toLocaleString("en-US");
+      break;
+    case 3:
+      if (activityCompleted) {
+        stringNum = "89%";
+      } else {
+        stringNum = number.toString() + "%";
+      }
+      break;
   }
 
   return stringNum;
 }
 
 export default function StatCard({ id, icon, alt, number, stat }) {
+  const { activityCompleted, quizScore } = useUserContext();
   return (
     <div className="statCardContainer">
       <img src={icon} alt={alt} />
-      <p className="number">{numberToString(id, number)}</p>
+      <p className="number">
+        {numberToString(id, number, activityCompleted, quizScore)}
+      </p>
       <p className="stat">{stat}</p>
     </div>
   );
