1d261707add2e2b9ba7b360714aa054e7ddcf59e
anabel <anabelcurry@gmail.com>
2025-12-02 10:16:09 -0800

    remove unneeded codes

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -3,22 +3,11 @@ import TeamProgress from "./TeamProgress";
 import StatCard from "./StatCard";
 import Leaderboard from "./Leaderboard";
 import { statCardContent } from "../../data/statCards";
-import { teamProgressContent } from "../../data/teamProgress";
 
 export default function Dashboard() {
   return (
     <>
       <TeamProgress />
-      <div>
-        {teamProgressContent.map(({ name, role, points, img}) => (
-          <StatCard
-            name={name}
-            role={role}
-            points={points}
-            img={img}
-          />
-        ))}
-      </div>
       <MissionStatus />
       <div>
         {statCardContent.map(({ id, icon, alt, number, stat }) => (
