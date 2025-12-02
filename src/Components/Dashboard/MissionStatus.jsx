fbc94385c7741445b00eb2abb961504fe0597d45
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-02 11:41:24 -0800

    Add Reward Unlocked card (#59)
    * Adds LottieFiles package
    
    * Adds elements and styles
    
    * fixes positioning
    
    * Removes redundant style
    
    * adds styling
    
    * Adds functionality
    
    * Adds mission completed check
    
    * Adds conditional rendering of mission status
    
    * Adds container for all mission status states

--- src/Components/Dashboard/MissionStatus.jsx
+++ src/Components/Dashboard/MissionStatus.jsx
@@ -1,3 +1,26 @@
+import RewardUnlocked from "./RewardUnlocked";
+import { useUserContext } from "../../Context/UserContext";
+
 export default function MissionStatus() {
-  return <h1>Start a New Mission</h1>;
+  const { activityCompleted, missionCompleted } = useUserContext();
+
+  if (missionCompleted) {
+    return (
+      <div className="missionStatusContainer">
+        <RewardUnlocked />
+      </div>
+    );
+  } else if (activityCompleted) {
+    return (
+      <div className="missionStatusContainer">
+        <h1>Mission Accomplished!</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="missionStatusContainer">
+        <h1>Start a New Mission</h1>
+      </div>
+    );
+  }
 }
