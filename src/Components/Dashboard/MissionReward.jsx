244c3a38940398bba7626a73c2cb0cd98ae642c7
Jenny C <jennyhc888@gmail.com>
2025-12-03 15:26:45 -0800

    added progress bar package

--- src/Components/Dashboard/MissionReward.jsx
+++ src/Components/Dashboard/MissionReward.jsx
@@ -1,6 +1,6 @@
 export default function MissionReward({}) {
   return (
-    <div className="mission-reward">
+    <div className="mission-reward-container">
       <h3>Mission Reward</h3>
       <p>{}</p>
     </div>
