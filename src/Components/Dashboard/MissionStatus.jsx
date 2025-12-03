af3e138d5053f67953737747cdb504d78f02e9de
Jenny C <jennyhc888@gmail.com>
2025-12-02 23:51:50 -0800

    Added components

--- src/Components/Dashboard/MissionStatus.jsx
+++ src/Components/Dashboard/MissionStatus.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import MissionStatusCarousel from "./MissionStatusCarousel";
+import MissionBriefingModal from "../../Components/MissionBriefingModal";
+import { useNavigate } from "react-router-dom";
+import "../../styles/MissionStatus.css";
+
 export default function MissionStatus() {
-  return <h1>Start a New Mission</h1>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleViewMission = () => {
+    navigate("/mission-hub");
+  };
+
+  return (
+    <section className="mission-status-section">
+      <div className="section-content">
+        <MissionStatusCarousel />
+      </div>
+      <div className="section-button-container">
+        <div className="button-briefing-container">
+          <button
+            className="view-briefing-button"
+            onClick={() => setIsModalOpen(true)}
+          >
+            View Mission Briefing
+          </button>
+        </div>
+        <div className="button-mission-container">
+          <button className="view-missions-button" onClick={handleViewMission}>
+            See All Missions
+          </button>
+        </div>
+        {isModalOpen && (
+          <MissionBriefingModal onClose={() => setIsModalOpen(false)} />
+        )}
+      </div>
+    </section>
+  );
 }
---
+++ src/Components/Dashboard/MissionStatusCarousel.jsx
@@ -0,0 +1,65 @@
+import MissionStatusCard from "./MissionStatusCard";
+import { useState } from "react";
+import { cardContent } from "../../data/missionHubCards";
+import leftArrow from "../../assets/icons/dashboard/leftArrow.svg";
+import rightArrow from "../../assets/icons/dashboard/rightArrow.svg";
+import completedIcon from "../../assets/icons/missionHub/completed.svg";
+import "../../styles/MissionStatusCarousel.css";
+
+export default function MissionStatusCarousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const incompleteMissions = cardContent.filter(
+    (card) => card.icon !== completedIcon
+  );
+
+  if (incompleteMissions.length === 0) {
+    return null;
+  }
+
+  const prevCard = () => {
+    setCurrentIndex((prev) =>
+      prev === 0 ? incompleteMissions.length - 1 : prev - 1
+    );
+  };
+
+  const nextCard = () => {
+    setCurrentIndex((prev) =>
+      prev === incompleteMissions.length - 1 ? 0 : prev + 1
+    );
+  };
+
+  const currentCard = incompleteMissions[currentIndex];
+  return (
+    <div className="mission-status-carousel">
+      <div className="carousel-header">
+        <h2>Start Your Mission</h2>
+      </div>
+
+      <div className="carousel-container">
+        <button onClick={prevCard} className="carousel-leftarrow">
+          <img src={leftArrow} className="leftarrow-img" alt="left arrow" />
+        </button>
+
+        <div className="card-stack">
+          <div className="background-card"></div>
+
+          <div className="carousel-content">
+            <MissionStatusCard
+              key={currentCard.id}
+              id={currentCard.id}
+              icon={currentCard.icon}
+              alt={currentCard.alt}
+              headline={currentCard.headline}
+              body={currentCard.body}
+            />
+          </div>
+        </div>
+
+        <button onClick={nextCard} className="carousel-rightarrow">
+          <img src={rightArrow} className="rightarrow-img" alt="right arrow" />
+        </button>
+      </div>
+    </div>
+  );
+}
