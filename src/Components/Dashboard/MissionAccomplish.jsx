d8159c00b8d64a630801a105b234d990de03405f
Jenny C <jennyhc888@gmail.com>
2025-12-03 21:55:12 -0800

    worked on Mission Accomplish section

--- src/Components/Dashboard/MissionAccomplish.jsx
+++ src/Components/Dashboard/MissionAccomplish.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import { statCardContent } from "../../data/statCards";
+import { useUserContext } from "../../Context/UserContext";
+import { Line } from "rc-progress";
+
 export default function MissionAccomplish() {
+  const { quizScore } = useUserContext();
+  const percent = statCardContent.find((card) => card.id === 3)?.number || 0;
+
   return (
     <div className="mission-accomplish-container">
-      <h1>Mission Reward</h1>
-      <p>{}</p>
+      <div className="mission-accomplish-header">
+        <h1>Mission Reward</h1>
+        <p>Your impact from completing Different Cultures Trivia</p>
+      </div>
+      <div className="points-added-display">
+        <span className="points-added">+{quizScore}</span>
+        <p>Points Added</p>
+      </div>
+      <div className="accomplish-progress-bar">
+        <div style={{ width: "300px" }}>
+          <Line
+            percent={percent}
+            strokeWidth={6}
+            strokeColor="#ff69b4"
+            trailColor="#f0f0f0"
+          />
+        </div>
+      </div>
     </div>
   );
 }
