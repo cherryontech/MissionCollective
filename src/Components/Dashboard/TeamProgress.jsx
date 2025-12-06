873a75ff2541a0ba3b7fbfe574917d7a33578e38
anabel <59778839+anabelcurry@users.noreply.github.com>
2025-12-05 16:44:07 -0800

    Dashboard Team Progress Section Styling (#66)
    * add styling to team progress section
    
    * fix incorrect import
    
    * address PR comments

--- src/Components/Dashboard/TeamProgress.jsx
+++ src/Components/Dashboard/TeamProgress.jsx
@@ -1,27 +1,30 @@
 import { teamProgressContent } from "../../data/teamProgress";
 import FilterArrow from "../../assets/icons/filterArrow.svg";
+import "../../styles/TeamProgress.css";
 
 export default function TeamProgress() {
   return (
     <div className="team-progress-container">
-      <h2>Your Team Progress</h2>
-      <div className="team-members">
-        {teamProgressContent.map(({ name, role, points, img }, idx) => (
-          <div className="team-member" key={`${name}-${idx}`}>
-            <img src={img} alt={name} />
-            <p className="member-name">{name}</p>
-            <p className="member-role">{role}</p>
-            <p className="member-points">{points}</p>
+        <h2>Your Team Progress</h2>
+        <div className="team-members">
+          {teamProgressContent.map(({ name, role, points, img }, idx) => (
+            <div className="team-member" key={`${name}-${idx}`}>
+              <img src={img} alt={name} />
+              <div className="member-text">
+                <p className="member-name">{name}</p>
+                <p className="member-role">{role}</p>
+              </div>
+              <p className="member-points">{points}</p>
+            </div>
+          ))}
+        </div>
+        <div className="team-progress-filter">
+          <p>Sort By</p>
+          <div className="filter-dropdown">
+            <p>Total Points in Last 30 Days</p>
+            <img src={FilterArrow} alt="filter-arrow"></img>
           </div>
-        ))}
-      </div>
-      <div className="team-progress-filter">
-        <p>Sort By</p>
-        <div classname="filter-dropdown">
-          <p>Total Points in Last 30 Days</p>
-          <img src={FilterArrow} alt="filter-arrow"></img>
         </div>
       </div>
-    </div>
   );
 }
