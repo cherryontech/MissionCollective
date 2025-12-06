3c569874c8a8bfc9c57a3f9be6af079ca5a39eca
anabel <anabelcurry@gmail.com>
2025-12-06 13:47:27 -0800

    adds final layout for all dashboard components

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -10,37 +10,41 @@ export default function Dashboard() {
   const { activityCompleted, missionCompleted } = useUserContext();
   return (
     <>
-      <TeamProgress />
-      <MissionStatus />
-      <div className="statCards">
-        {statCardContent.map(
-          ({
-            id,
-            icon,
-            alt,
-            initialNumber,
-            activityNumber,
-            rewardNumber,
-            stat,
-          }) => (
-            <StatCard
-              key={id}
-              id={id}
-              icon={icon}
-              alt={alt}
-              number={
-                missionCompleted
-                  ? rewardNumber
-                  : activityCompleted
-                    ? activityNumber
-                    : initialNumber
-              }
-              stat={stat}
-            />
-          )
-        )}
+      <div className="dashboardContainer">
+        <div className="progressMissionSection">
+        <TeamProgress />
+        <MissionStatus />
+        </div>
+        <div className="statCards">
+          {statCardContent.map(
+            ({
+              id,
+              icon,
+              alt,
+              initialNumber,
+              activityNumber,
+              rewardNumber,
+              stat,
+            }) => (
+              <StatCard
+                key={id}
+                id={id}
+                icon={icon}
+                alt={alt}
+                number={
+                  missionCompleted
+                    ? rewardNumber
+                    : activityCompleted
+                      ? activityNumber
+                      : initialNumber
+                }
+                stat={stat}
+              />
+            )
+          )}
+        </div>
+        <Leaderboard />
       </div>
-      <Leaderboard />
     </>
   );
 }
