bff734db6f09cd12ee6e6609cff4c80b54ca2f7e
jennycodingnow <101082675+jennycodingnow@users.noreply.github.com>
2025-12-06 16:40:39 -0800

    Mission-Status-Section-Dashboard (#65)
    * added icons
    
    * Added components
    
    * Added stylings
    
    * Resolved comments on PR
    
    * organize components in the dashboard
    
    * combined and fixed mission status section
    
    * Fixed header

--- src/Components/Dashboard/MissionStatus.jsx
+++ src/Components/Dashboard/MissionStatus.jsx
@@ -1,4 +1,5 @@
 import RewardUnlocked from "./RewardUnlocked";
+import MissionStart from "./MissionStart";
 import { useUserContext } from "../../Context/UserContext";
 
 export default function MissionStatus() {
@@ -19,7 +20,7 @@ export default function MissionStatus() {
   } else {
     return (
       <div className="missionStatusContainer">
-        <h1>Start a New Mission</h1>
+        <MissionStart />
       </div>
     );
   }
